ca99702117b448bd6bbbe88bd6c9632f5ace6c6e
agam72 <51700742+agam72@users.noreply.github.com>
2019-07-01 18:22:36 +0530

    Add files via upload

---
+++ to-do-list2.js
@@ -0,0 +1,60 @@
+let todo=[]
+
+
+
+
+const render=(state)=>{
+    return state.reduce((acum,todo,index)=>{
+        const string=todo.striked ? 'class= "striked"': ''
+        return acum + `<li ${string} data-index="${index}" > ${todo.title} </li>`
+    }, '')
+
+    
+}
+
+const paint = function() {
+    const html= render(todo)
+    document.getElementById("list").innerHTML=html
+}
+
+document.getElementById("addbutton").addEventListener("click", e =>{
+    const input=document.getElementById("in").value 
+    document.getElementById("in").value =""
+    todo.push({
+        title: input,
+        striked: false
+    })
+    paint()
+    localStorage.removeItem("todo");
+localStorage.setItem("todo", JSON.stringify(todo));
+    
+})
+
+document.getElementById("list").addEventListener("click",e =>
+{
+    const index = e.target.dataset.index
+    todo[index].striked=!todo[index].striked
+    
+    paint()
+    localStorage.removeItem("todo");
+localStorage.setItem("todo", JSON.stringify(todo));
+})
+
+
+document.getElementById("remove").addEventListener("click",e=>{
+  todo=todo.filter(item=> !item.striked)
+    paint()
+    localStorage.removeItem("todo");
+localStorage.setItem("todo", JSON.stringify(todo));
+})
+
+let temp=JSON.parse(localStorage.getItem("todo"))
+if(temp===null)
+{
+
+}
+else{
+    todo=temp
+    paint()
+}
+
